cb309a48502e675d3666530bc3990c89a479a893
Ankit Khushal Barai <ankitbarai507@gmail.com>
2020-06-26 14:47:23 +0530

    Add files via upload

--- generate_ebook_patching.py
+++ generate_ebook_patching.py
@@ -1,9 +1,28 @@
-from pdf2image import convert_from_path, convert_from_bytes
+# from pdf2image import convert_from_path, convert_from_bytes
 
-image_invoices = convert_from_bytes(open('invoice/'+str(j)+".pdf", 'rb').read())
-j=1
-for image in image_invoices:
-    image_invoice[0].save('invoice/'+str(j)+".png")
-    j+=1
-    if j>20:
-        break
+# image_invoices = convert_from_bytes(open('aho_ulman.pdf', 'rb').read())
+# j=1
+# for image in image_invoices:
+#     image.save('invoice/'+str(j)+".png")
+#     print(image.shape)
+#     j+=1
+#     if j>20:
+#         break
+
+#install PyMupdf
+import fitz
+import sys
+
+pdffile = sys.argv[1]
+low=int(sys.argv[2])
+high=int(sys.argv[3])
+op=sys.argv[4]
+doc = fitz.open(pdffile)
+
+for i in range(low,high):
+    page = doc.loadPage(i) #number of page
+    zoom = 4.5    # zoom factor
+    mat = fitz.Matrix(zoom, zoom)
+    pix = page.getPixmap(matrix = mat)
+    output = op+str(i)+".png"
+    pix.writePNG(output)
